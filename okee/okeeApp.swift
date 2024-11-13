8c028720ac675f47f6cce662cc412e77c5e7c0cb
alishancode <alishan@byldd.com>
2024-11-13 10:46:26 +0530

    Initial Commit

---
+++ okee/okeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  okeeApp.swift
+//  okee
+//
+//  Created by Md Alishan Ali on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct okeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ okeeTests/okeeTests.swift
@@ -0,0 +1,17 @@
+//
+//  okeeTests.swift
+//  okeeTests
+//
+//  Created by Md Alishan Ali on 13/11/24.
+//
+
+import Testing
+@testable import okee
+
+struct okeeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
